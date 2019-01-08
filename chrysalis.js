82bb5dfb6925e5764fa873710cfe33bf332a5bff
Abigail Brady <abigail.brady@filigree.tech>
2019-01-08 15:18:11 +0000

    needs to be a module, apparently

--- chrysalis.js
+++ chrysalis.js
@@ -1,42 +1,44 @@
 
-  import { handleTerminal } from "./terminal.js" 
-  import { handleTelnet } from "./telnet.js" 
+import { handleTerminal } from "./terminal.js" 
+import { handleTelnet } from "./telnet.js" 
 
-  const ws = new WebSocket("ws://192.168.0.31:8080");
-  ws.binaryType = "arraybuffer";
+const ws = new WebSocket("ws://127.0.0.1:8080");
+ws.binaryType = "arraybuffer";
 
-  ws.onmessage = function(event) {
-    const arr = new Uint8Array(event.data);
-    arr.forEach(ch => handleTelnet(ch, handleTerminal))
-  }
+ws.onmessage = function(event) {
+  const arr = new Uint8Array(event.data);
+  arr.forEach(ch => handleTelnet(ch, handleTerminal))
+}
 
-  ws.onopen = function(_) {
-  }
+ws.onopen = function(e) {
+  console.debug(e)
+}
   
-  ws.onclose = function(_) {
-  }
-  
-  const input = document.getElementById("input")
+ws.onclose = function(e) {
+  console.debug(e)
+}
+ 
+const input = document.getElementById("input")
 
-  input.onkeypress = function(event) {
-    if (event.key === "Enter") {
-      console.log(input.value)
+input.onkeypress = function(event) {
+  if (event.key === "Enter") {
+    console.log(input.value)
 
-      const cmd = []
-      const value = input.value.toString()
+    const cmd = []
+    const value = input.value.toString()
 
-      for (let idx = 0; idx < value.length; idx += 1) {
-        cmd.push(value.charCodeAt(idx))
-      }
-
-      cmd.push(10)
-      cmd.push(13)
-
-      ws.send(new Uint8Array(cmd))
-
-      input.value = ""
+    for (let idx = 0; idx < value.length; idx += 1) {
+      cmd.push(value.charCodeAt(idx))
     }
+
+    cmd.push(10)
+    cmd.push(13)
+
+    ws.send(new Uint8Array(cmd))
+
+    input.value = ""
   }
+}
 
   
 
