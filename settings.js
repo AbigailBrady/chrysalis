e2b7c32346cd0d1502ef225ee01166a5c6c961b9
Abigail Brady <abigail.brady@filigree.tech>
2019-01-10 15:18:10 +0000

    don't reassign entire innerHTML each time, just append to it. handle prompts.

---
+++ settings.js
@@ -0,0 +1,2 @@
+export const url = "ws://127.0.0.1:8081"
+
---
+++ telnetconstants.js
@@ -0,0 +1,17 @@
+export const IAC = 255;
+export const DONT = 254;
+export const DO = 253;
+export const WONT = 252;
+export const WILL = 251;
+export const SB = 250
+export const NOP = 241;
+export const SE = 240;
+export const EOR = 239;
+
+export const ECHO = 1;
+export const TTYPE = 24;
+export const TELOPT_EOR = 25;
+
+export const SEND = 1;
+export const IS = 0;
+
